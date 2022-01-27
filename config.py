d9c88ab7f4b0c680ce4e0793d2f0d592abd06365
mkjuice <89534948+mkjuice@users.noreply.github.com>
2022-01-27 16:12:16 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+TOKEN='OTM1MjI4MTQ5MDU3MzM5NTIz.Ye7lFg.qwevngodLprFa8q0hB9S4E5Sl_E'
+
+firebaseconfig= {
+  "type": "service_account",
+  "project_id": "cryptogene-7bcbb",
+  "private_key_id": "e26fd789d423b006b2e6e5009879b0e403c4c279",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDlqFbLQTK6jmUS\nytPpaNGD3ODCHGHKnJyckex5QAxazoZBXGVchi7W+oGGyDUsrZq/d/POqRTAgNfr\nNmJTOtgoOxDKEajNseBjD1C6v1tIQqX+ludadKgrAbvcUl36MpiuWLhMxoMH6IDN\n6Piq/bjO/0ZssT9FWZyZSPQ2KzXcctTUDkp14MvHlQFBMafyRagrs8Z/vnfRs3nv\n1T0koD/dHWtBcuOjJysOiBZTj9AHfaNh+0cbIRvUH6wlmoQ7FCz7/31XhZbwvDgS\n3GIs8zk/yL33835jMq7JENFebaUUgvgdhsQvh9BDHQo/G3btOdhN6Zlf6aXHhkl7\nGCVt2GaDAgMBAAECggEABUTE5PnUb5o3sGQt02N/tv44u8tJ5QuNvjvMyIVTIzws\nsVAs1SAQ8AakNRLfwWwkKjNWyQ1w9++cxH0laVJmxhmAy5n3HOzxQGSIDhi4v+Ij\ni8ErgpQ9jWsHQ8kx9bz2ar2HUN+W/vXzCoZHOikC9eC+XVz9nhotsQiO7bU1EZ8X\nqbv40LvhQWPzd5iNzTjCL6yZmnSrks+bed50l39MYB9tU7iONrOVmXs6JJZ8ZIdM\nE/A2h/9Y5ij/LnctdSoKb00Dlnwy/j9OkptMQZBF4M839GGRunWzWn93EAR+4ZFB\nt2ej5j7dWnmrj3MuL2Tf+eeE7qm1Qa4HzoefPi4gKQKBgQDzpayegErKkeJPhT0j\noRe7BnpcNvuoFqFpcDWKZGeA2XChnE/z5a/ISqGkQP8ocPzW8qHwKPOzhB/8iUMM\nxBTmrprhGRhDmgasB0LapkP07ZxikpU0DxzblLo23xHvqjcoxJVOlLlqri4ii5t0\nBUo45oinahf0DTSWA46ph++BmwKBgQDxTReYmURG+WMz5arkjri2KOvlNY8hNTVC\n5v0uzNceSyL2kivjAuKwe2Ua10oU0DEt9Y6N0nFc0WvVgIbQiutASOO8QTAIQk+W\nGx3EVfuOtvTN4pjt889dg7b3Nk2CJ0WL+nfV04vN+/EymWhn8rznjOyCO4VBy3es\nY/fbc+/ROQKBgC1y7SVP5yOsXNvajdIUnbmcdry24PC07/0cGlCrdAc8CF+NKfjk\nEos5KLfcC/y+PtvVvNFwwizta44PCLvb4844KdntdXI+zBnQ7O6ebzTEEhTtJPnm\nC8sGDkpoCA993/Yx3WwgIAo/XYnCIwhcaED+9+TKzJcQ04tcNIN5QPsbAoGBAL2Y\nm1+1naY+sanHXX10fsHyaVeRu8KV8iF9KZYqMSSGWRRwSbd3PvL4IbrzSiwMYXgB\nJ19b6OLLUfVRpZwaazn5i6cReVIj+DCzYbIw9RVvv+49i3f8SATsR0j7C++mN8kC\npkIGlCTnPt9JFnYGjlNJsVOqvMuzuXZkng16jQZRAoGBAIhAvKR8sl8p0F7Z/tE7\ntPviIURY2IaytYG3mLwp0ldMyVEeSGYoy8V8xpnAhDrIpxDohg/VNv6BWxKjIQI9\nBBfD6aNa2JTGb93J8Sf/SQCTtZzzr1jFPnq0H6TTDGdnASxKZYkq+12VhKBwoG6o\nJEaSXTkHGA4anI83P5zXVRTK\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-tzv8d@cryptogene-7bcbb.iam.gserviceaccount.com",
+  "client_id": "104195614282808283122",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-tzv8d%40cryptogene-7bcbb.iam.gserviceaccount.com"
+}
+
+databaseURL="https://cryptogene-7bcbb-default-rtdb.firebaseio.com/"
---
+++ main.py
@@ -0,0 +1,116 @@
+from __future__ import print_function
+
+import discord
+from discord.ext import commands
+import time
+from config import *
+
+import firebase_admin
+from firebase_admin import auth
+from firebase_admin import credentials
+from firebase_admin import db
+
+
+
+cred = credentials.Certificate(firebaseconfig)
+databaseApp = firebase_admin.initialize_app(cred,{'databaseURL':databaseURL})
+
+#client = discord.Client()
+
+bot = commands.Bot(command_prefix="!", description="game bot")
+
+
+# @client.event
+# async def on_message(message):
+
+#     if message.content.startswith('!whitelist'):
+#         personid = str(message.author.id)
+#         person=str(message.author.name)
+#         rows = read_range()
+#         print(rows)
+#         addy=str(message.content).split(" ",1)[1]
+#         print('addy is: '+str(addy))
+#         if str(personid) not in rows:
+
+#           DATA = [message.author.name] + [str(message.author.id)] + [str(message.created_at)] + [str(message.content).split(" ",1)[1]]
+#           add(SPREADSHEET_ID, RANGE_NAME, DATA)
+#           await message.channel.send(str(person) + ': ' + str(addy) +' has been whitelisted!')
+
+#         if str(personid) in rows:
+#           print('already registered')
+#           await message.channel.send(str(person)+': is already whitelisted!')
+#         #await message.channel.send("hello!")
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+
+# @bot.command(pass_context=True)
+# async def write(ctx): # !write
+#     #color = input("pick a color")
+#     person= ctx.message.author
+#     user = ctx.message.id
+#     print(user, " is user id")
+#     ref = db.reference(f"/")
+#     ref.update({
+#         user: "nope"
+#         }
+#     )
+#     await message.channel.send(str(person)+", your login code is: "str(user))
+
+@bot.command(pass_context=True)
+async def play(ctx): # !play
+    #color = input("pick a color")
+    person= ctx.message.author
+    user = ctx.message.author.id
+    print(user, " is user id")
+    ref = db.reference(f"/")
+    ref2 = db.reference(f"/"+"bobby") 
+    status2 = ref2.get()
+    #print(status2)
+    if status2 == None:
+        ref.update({
+            user: "nope"
+            }
+    )
+    await ctx.channel.send(str(person)+", your login code is: "+str(user))
+
+def letsee():
+    ref2 = db.reference(f"/"+"bobby") 
+    status2 = ref2.get()
+    print(status2)
+    if status2 == None:
+        print("caught")
+
+
+letsee()
+
+@bot.command(pass_context=True)
+async def upgrade(ctx): #!upgrade
+    person= ctx.message.author
+    user = ctx.message.author.id
+    ref = db.reference(f"/"+str(user))    
+    status = ref.get()
+    print("status: "+ str(status))
+
+    rolelist=[]
+    for role in person.roles:
+        rolelist.append(role.name)
+    print(rolelist)
+
+    if status == "nope":
+        await ctx.channel.send(str(person) + ", you haven't reached the score threshold!")
+    if int(status) >=15:
+        if "bear" not in rolelist:
+            ogrole = discord.utils.get(person.guild.roles, name = "bear")
+            await person.add_roles(ogrole, atomic=True)
+            await ctx.channel.send(str(person) + ", congrats! Your role is now OG.")
+        else:
+            await ctx.channel.send(str(person) + ", You already have OG role")
+    else:        
+        await ctx.channel.send(str(person)+ ", please use the !play command to get your game code.")    
+    
+
+bot.run(TOKEN)    
